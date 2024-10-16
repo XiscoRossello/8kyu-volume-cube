624f4d53111fdaa706c778449321920989271a0f
Francisco Javier Rossello Jeronimo <frossellojeronimo@cifpfbmoll.eu>
2024-10-16 10:49:40 +0200

    Create volume.java

---
+++ volume.java
@@ -0,0 +1,10 @@
+// creamos un programa que devuelva el area de un cubo
+
+public class volume {
+
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    double volume = length * width * height;
+    return volume;
+  }
+  
+}
